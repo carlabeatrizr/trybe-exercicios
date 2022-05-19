ccb9713cb50cfdfc1bbb3e8cda262f42ae1ded83
Carla Beatriz Rabelo <carlabeatrizrabelo@gmail.com>
2022-05-19 12:32:39 -0300

    Add alterações no exercício 11 de 4.1

--- fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/4.1/exercicio-11/script.js
+++ fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/4.1/exercicio-11/script.js
@@ -1,6 +1,6 @@
-const anguloA = 60;
-const anguloB = 50;
-const anguloC = 10;
+const anguloA = 80;
+const anguloB = 30;
+const anguloC = 70;
 
 let somaDosAngulos = anguloA + anguloB + anguloC;
 
@@ -8,7 +8,7 @@ let angulosQueSaoPositivos =
 anguloA > 0 && anguloB > 0 && anguloC > 0;
 
 if (angulosQueSaoPositivos) {
-    if (somaDosAngulos === 120) {
+    if (somaDosAngulos === 180) {
         console.log(true);
 } else {
     console.log(false);
