e4307aca05f1b67e8181f603a6ead1ff84004448
Carla Beatriz Rabelo <carlabeatrizrabelo@gmail.com>
2022-05-19 11:37:32 -0300

    Add exercício 9 de 4.1

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/4.1/exercicio-9/script.js
@@ -0,0 +1,13 @@
+const a = 10
+const b = 50
+const c = 30
+
+if(a > b && a > c){
+    console.log("O maior numero é:" + a);
+}
+else if(b > a && b > c){
+    console.log("O maior numero é:" + b);
+}
+else{
+    console.log("O maior numero é:" + c);
+}
