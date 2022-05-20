400360a2cd98e23a0b1a76ff4942e0b5b56f2049
Carla Beatriz Rabelo <carlabeatrizrabelo@gmail.com>
2022-05-20 13:11:03 -0300

    Add exercício 17 de 4.1

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/4.1/exercicio-17/scrpt.js
@@ -0,0 +1,30 @@
+let aliquotaINSS;
+let aliquotaIR;
+
+const salarioBruto = 9000.0;
+
+if(salarioBruto <= 1556.94){
+    aliquotaINSS = salarioBruto * 0.08;
+} else if(salarioBruto <= 2594.92){
+    aliquotaINSS = salarioBruto * 0.09;
+}else if(salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * 0.11;
+}else{
+    aliquotaINSS = 570.88
+}
+
+const salarioBase = salarioBruto - aliquotaINSS;
+
+if(salarioBase <= 1903.98){
+    aliquotaIR = 0;
+}else if(salarioBase <= 2826.65){
+    aliquotaIR = salarioBase * 0.075 - 142.8;
+}else if(salarioBase <= 3751.05){
+    aliquotaIR = salarioBase * 0.15 - 354.8;
+}else if(salarioBase <= 4664.68){
+    aliquotaIR = salarioBase * 0.225 - 636.13;
+} else{
+    aliquotaIR = salarioBase * 0.275 - 869.36;
+}
+
+console.log("Salário: " + (salarioBase - aliquotaIR));
