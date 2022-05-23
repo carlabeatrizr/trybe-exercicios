28da31073b478c815b8024871df136ad457a95c9
Carla Beatriz Rabelo <carlabeatrizrabelo@gmail.com>
2022-05-23 19:30:14 -0300

    Add exercicio 1 de 4.1

---
+++ fundamentos/bloco-4-introducao-javascript-e-logica-de-programacao/4.2/exercicio-1/script.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+console.log(numbers);
+
+let sum = 0;
+
+for (let index = 0; index < 10; index++) {
+
+    sum += numbers[index];
+
+}
+console.log(sum);
